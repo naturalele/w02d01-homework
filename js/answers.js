9e53b9823d06962dc512838d8c096055b7b5622b
Kathleen Turner <kate@Kathleens-MacBook-Pro.local>
2018-06-18 20:59:52 -0600

    1st commit - selecting datatypes

---
+++ js/answers.js
@@ -0,0 +1,29 @@
+console.log("peace");
+
+// Data Type
+
+// 1. Booleans
+// ex. if(light == true) {
+// 	console.log("Light is on")
+// };
+
+// 2. String ex. ("inspiredfire@gmail.com")
+
+// 3. Array 
+// const spaceship = ["hull", "laserBlasters", "tractorBeam"];
+
+// 4. Array
+// const class = ["tim", "karen", "steve"];
+
+// 5. Object
+// const class = {
+// 	student: [],
+// 	location: []
+// };
+
+// 6. Object
+const class = {
+	student: [],
+	location: [],
+	favoriteTvShow: []
+};
